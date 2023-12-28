36831c33f450ea7c3deea19eed0b6ca7d71a883a
Denis <109104246+Denis0014@users.noreply.github.com>
2023-12-28 22:09:11 +0300

    Update pram.py

--- pram.py
+++ pram.py
@@ -1,9 +1,10 @@
-print(2)
-for i in range (3, 100):
-    flag = True
-    for j in range (2, i):
-        if i % j == 0:
-            flag = False
-            break
-    if flag:
-        print(i)
+def print():
+    print(2)
+    for i in range (3, 100):
+        flag = True
+        for j in range (2, i):
+            if i % j == 0:
+                flag = False
+                break
+        if flag:
+            print(i)
