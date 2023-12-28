d5201aa9117b259cce4e3b31b2b45f4fabc3e34f
Denis <109104246+Denis0014@users.noreply.github.com>
2023-12-28 21:29:44 +0300

    Update pram.py

--- pram.py
+++ pram.py
@@ -2,7 +2,7 @@ print(2)
 for i in range (3, 100):
     flag = True
     for j in range (2, i):
-        if i % j == 0:
+        if i % k == 0:
             flag = False
             break
     if flag:
